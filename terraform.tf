65c635163f8f883c6d76093d6fbc79bbb2fc334e
Matteo Gastaldello <matteo.gastaldellomiotto@gmail.com>
2024-05-30 17:05:32 +0200

    k8s backend

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "kubernetes" {
+    secret_suffix    = "aws-sqs"
+    namespace = "default"
+    in_cluster_config  = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
